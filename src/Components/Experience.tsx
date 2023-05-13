7c0c57f157a8d4ac13e1cdf1d85fd0c45ec56bf0
Saba Phruidze <sabaphruidze01@gmail.com>
2023-05-13 15:47:12 +0400

    changes

--- src/Components/Experience.tsx
+++ src/Components/Experience.tsx
@@ -1,81 +1,26 @@
 import React from "react";
-import { createContext, useContext } from "react";
-import { context } from "../App";
+import { useContext, createContext, useState, useRef } from "react";
 
 import CommonStyles from "./Common.module.css";
+import PersonalStyles from "./Personal.module.css";
 
-import Ellipse from "../Assets/Img/2. SecondPage/Ellipse.svg";
-import Vector from "../Assets/Img/2. SecondPage/Vectorvector.svg";
-export const MyPersonalContext1 = createContext<any>("s");
-
+export const MyExperienceLeftSideContext = createContext<any>("w");
 export default function Experience() {
-  const useAppContext2 = useContext(context);
-  const {
-    cPage,
-    sPage,
-    cLanguage,
-    sLanguage,
-    cBGColor,
-    sBGColor,
-    cName,
-    sName,
-    cUsername,
-    sUsername,
-    cUploadImg,
-    sUploadImg,
-    cEmail,
-    sEmail,
-    cTel,
-    sTel,
-    cAboutMe,
-    sAboutMe,
-    cCorrect,
-    sCorrect,
-    cCorrect1,
-    sCorrect1,
-    cCorrect2,
-    sCorrect2,
-    cCorrect3,
-    sCorrect3,
-    cVisible,
-    sVisible,
-  } = useAppContext2;
-
   return (
-    <div className={CommonStyles.infoContainer}>
-      <img
-        src={Ellipse}
-        alt="Ellipse"
-        className={CommonStyles.ellipse}
-        onClick={() => useAppContext2.sPage(1)}
-      />
-      <img
-        src={Vector}
-        alt="Vector"
-        className={CommonStyles.vector}
-        onClick={() => useAppContext2.sPage(1)}
-      />
-      <div className={CommonStyles.skeleton}>
-        <div className={CommonStyles.dFlex}>
-          <p
-            style={{
-              fontSize: "24px",
-              marginBottom: "12px",
-              fontWeight: "700",
-            }}
-          >
-            გამოცდილება
-          </p>
-          <p
-            style={{
-              fontSize: "20px",
-            }}
-          >
-            1/3
-          </p>
-        </div>
-        <hr />
-      </div>
+    <div
+      style={{
+        width: 1098,
+        height: 1080,
+        backgroundColor: "#f9f9f9",
+        color: "black",
+        position: "absolute",
+        zIndex: "1",
+        top: 0,
+        left: 0,
+      }}
+    >
+      width:1098px; height:1080px; background-color: #090909; color:white;
+      position: absolute; z-index: 1; top:0; left:0; fewf
     </div>
   );
 }
