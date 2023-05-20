fc6ba7e3c0b82c008750e9bba366850bb9f40d21
Saba Phruidze <sabaphruidze01@gmail.com>
2023-05-20 21:08:08 +0400

    Tommorow is the last page day

--- src/Components/FinalResume.tsx
+++ src/Components/FinalResume.tsx
@@ -3,8 +3,8 @@ import { useContext } from "react";
 import { context } from "../App";
 
 import CommonStyles from "./Styles/Common.module.css";
-
+import RightSide from "./InfoUpdateComponents/RightSide";
 export default function FinalResume() {
   const useAppContext4 = useContext(context);
-  return <div>{useAppContext4.cBGColor ? "FinalResume" : "FinalResume"}</div>;
+  return <div className={CommonStyles.finalResume}>საბოლოო</div>;
 }
